fbcf5755d1295492d16e57b6b75566c970ff2a64
Eliz44 <148425316+Eliz44@users.noreply.github.com>
2023-11-16 10:21:35 +0200

    Add files via upload

---
+++ 1uzdevums.py
@@ -0,0 +1,12 @@
+
+lietotaja_skaitlis = int(input("Ievadiet skaitli: "))
+
+
+summa = 0
+
+
+for i in range(1, lietotaja_skaitlis + 1):
+    summa += i
+
+
+print(f"Summa no 1 līdz {lietotaja_skaitlis} ir: {summa}")
